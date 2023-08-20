d14e54687957c5e964384e3276b2041efc44af85
willianfrancas <willianfrancas@gmail.com>
2023-08-20 14:55:58 -0300

    add karma file

---
+++ karma.conf.js
@@ -0,0 +1,54 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/0.13/config/configuration-file.html
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+
+    client: {
+      jasmine: {},
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+
+    jasmineHtmlReporter: {
+      supressAll: true // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/wilianfrancas'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, 'coverage'), reports: ['html', 'lcovonly'],
+      fixWebpackSourcePaths: true
+    },
+
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome', 'ChromeHeadlessCI'],
+    singleRun: false,
+    restartOnFileChange: true,
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    }
+  });
+};
