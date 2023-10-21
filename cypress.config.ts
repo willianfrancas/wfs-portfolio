a6f8e4edc525231bbfc7a5f41416f96d3d07edbf
willianfrancas <willianfrancas@gmail.com>
2023-10-21 13:26:58 -0300

    :sparkles: increase viewport

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,21 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  
+
   e2e: {
     'baseUrl': 'http://localhost:4200'
   },
-  
-  
+
+
   component: {
     devServer: {
       framework: 'angular',
       bundler: 'webpack',
     },
     specPattern: '**/*.cy.ts'
-  }
-  
+  },
+
+  viewportHeight: 720,
+  viewportWidth: 1440,
+
 })
