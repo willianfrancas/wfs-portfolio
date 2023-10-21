310383ae9a37e658364056ae63f67049d9da8a62
willianfrancas <willianfrancas@gmail.com>
2023-10-21 13:27:40 -0300

    :white_check_mark: add test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,23 @@
-describe('My First Test', () => {
+beforeEach(() => {
+  cy.visit('http://localhost:4200/')
+});
+
+describe('Website is On', () => {
   it('Visits the initial project page', () => {
-    cy.visit('http://localhost:4200/')
     cy.get('.hero-container > h1').should('contain', 'William F. Silva');
-  })
-})
+  });
+});
+
+describe('Count elements', () => {
+  it('should contain 6 items on menu', () => {
+    const itemsArray = ['Home', 'About', 'Skills', 'License & Certified', 'Resume', 'Contact'];
+    cy.get('#navbar ul li a i~span').then(content => {
+      cy.wrap(content).should('have.length', 6);
+      for (let i = 0; i < content.length; i++) {
+        const element = content[i];
+        cy.wrap(element).should('contain.text', itemsArray[i]);
+      }
+    });
+  });
+});
+
